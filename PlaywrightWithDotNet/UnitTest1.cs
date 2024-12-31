5d217a8d155e54594ce293ed268c56e6023bfa1f
surya <soni.surya84@gmail.com>
2024-12-31 12:51:16 +0530

    added required files for framework

---
+++ PlaywrightWithDotNet/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace PlaywrightWithDotNet;
+using Microsoft.Playwright;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        using var playwright = await Playwright.CreateAsync(); 
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions{Headless = false});
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("https://playwright.dev");
+        await page.PauseAsync();
+
+    }
+}
